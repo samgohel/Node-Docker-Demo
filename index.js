344fbb4259f4fc6d36f35382b9acc1420cfde521
Shambhu Gohel <gohelshambhu95@yahoo.com>
2021-05-03 13:21:02 +0530

    Created Basic Node App

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h2> Hi There! Shambhu <h2>");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening On PORT ${port}`));
